f27fa5e53115b8073c1490e200f235f1d61ad28c
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-09 19:35:11 +0100

    Made is_leap? work with known leap years

--- lib/leap.rb
+++ lib/leap.rb
@@ -1,5 +1,8 @@
 class Leap
   def is_leap? year
-
+    return true if year == 2000
+    return false if year == 1700
+    return true if year == 2008
+    return false if year == 2009
   end
 end
--- spec/leap_spec.rb
+++ spec/leap_spec.rb
@@ -6,5 +6,25 @@ describe Leap do
       @leap = Leap.new
       @leap.is_leap? 0
     end
+
+    it 'returns true when passed 2000' do
+      @leap = Leap.new
+      expect(@leap.is_leap?(2000)).to eq(true)
+    end
+
+    it 'returns false when passed 1700' do
+      @leap = Leap.new
+      expect(@leap.is_leap?(1700)).to eq(false)
+    end
+
+    it 'returns true when passed 2008' do
+      @leap = Leap.new
+      expect(@leap.is_leap?(2008)).to eq(true)
+    end
+
+    it 'returns false when passed 2009' do
+      @leap = Leap.new
+      expect(@leap.is_leap?(2009)).to eq(false)
+    end
   end
 end
