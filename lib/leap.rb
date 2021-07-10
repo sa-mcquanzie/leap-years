980e8eaa42c0db34982e240a9c56a11e95d50fd3
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-07-10 13:48:47 +0100

    Refactored is_leap? method

--- lib/leap.rb
+++ lib/leap.rb
@@ -1,8 +1,5 @@
 class Leap
   def is_leap? year
-    return true if year % 400 == 0
-    return false if year % 100 == 0 && year % 400 != 0
-    return true if year % 4 == 0 && year % 100 != 0
-    return false if year % 4 != 0
+    year % 400 == 0 || (year % 4 == 0 && year % 100 != 0) ? true : false
   end
 end
